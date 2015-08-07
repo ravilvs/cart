666063b880f43ae441d3b459b91722037b250970
ravi <ravilvs@gmail.com>
2015-08-07 04:59:24 +0000

    changed

--- variables.php
+++ variables.php
@@ -2,9 +2,32 @@
 		$_SESSION['server'] = '127.6.94.130';
 		$_SESSION['user'] = 'adminqDjdZee';
 		$_SESSION['password'] = 'bwGDtxB_N8-I';
-		$_SESSION['database'] = 'ravi__';
+		$_SESSION['database'] = 'ravi';
 		$_SESSION['userid'] = 0;
 		$_SESSION['login'] = 0;
 
+			$sqlcon = mysqli_connect($_SESSION['server'], $_SESSION['user'], $_SESSION['password'], $_SESSION['database']);
+			if(!$sqlcon)
+			{
+				die("Error in connecting database");
+			}
+			
+			$result = $sqlcon->query("select * from product groups");
+			if(!$result)
+			{
+    			while($row = $result->fetch_array())
+    			{
+    			    echo 'Table : ';
+			        echo $row[0];
+			        echo '<br />';
+			        
+    			}
+    			
+			}
+			else
+			{
+			    
+			    print_r(error_get_last());
+			}
 
 ?>
