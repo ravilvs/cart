e67d3f9f4a8558eed15d53a5fd70b85504cf606d
ravi <ravilvs@gmail.com>
2015-08-07 04:25:55 +0000

    changed

--- variables.php
+++ variables.php
@@ -10,10 +10,10 @@
         define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
         define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 	    
-		$_SESSION['server'] = constant("DB_HOST");
-		$_SESSION['user'] = constant("DB_USER");
-		$_SESSION['password'] = constant("DB_PASS");;
-		$_SESSION['database'] = constant("DB_NAME");;
+		$_SESSION['server'] = '127.6.94.130';
+		$_SESSION['user'] = 'adminqDjdZee';
+		$_SESSION['password'] = 'bwGDtxB_N8-I';
+		$_SESSION['database'] = 'ravi';
 		$_SESSION['userid'] = 0;
 		$_SESSION['login'] = 0;
 	}
