1b0d9b9bc4fd1ec83b40aca6ea6095774922438f
ravi <ravilvs@gmail.com>
2015-08-07 04:28:06 +0000

    changed

--- variables.php
+++ variables.php
@@ -4,11 +4,7 @@
 	if(!isset($_SESSION['server']))
 	{
 	    
-	    define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-        define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-        define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-        define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-        define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+	    echo 'Session Created';
 	    
 		$_SESSION['server'] = '127.6.94.130';
 		$_SESSION['user'] = 'adminqDjdZee';
