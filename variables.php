92bdb48fbd658463e0035e46073460e707a4c2b6
ravi <ravilvs@gmail.com>
2015-08-07 04:55:31 +0000

    changed

--- variables.php
+++ variables.php
@@ -2,7 +2,7 @@
 		$_SESSION['server'] = '127.6.94.130';
 		$_SESSION['user'] = 'adminqDjdZee';
 		$_SESSION['password'] = 'bwGDtxB_N8-I';
-		$_SESSION['database'] = 'ravi';
+		$_SESSION['database'] = 'ravi__';
 		$_SESSION['userid'] = 0;
 		$_SESSION['login'] = 0;
 
